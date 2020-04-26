fe56955e52888d32ed080b5d00ee598a593082b1
Sovuhin <all-light1@mail.ru>
2020-04-26 20:04:22 +0300

    django
    jquery

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -4,6 +4,7 @@ from urllib import request
 from django.core.files.base import ContentFile
 from django.utils.text import slugify
 
+
 class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
@@ -20,10 +21,15 @@ class ImageCreateForm(forms.ModelForm):
 
     def save(self, force_insert=False, force_update=False, commit=True):
         image = super(ImageCreateForm, self).save(commit=False)
-        image_url=self.cleaned_data['url']
-        image_name = '{}.{}'.format(slugify(image.title), image_url.rsplit('.', 1)[1].lower())
+        image_url = self.cleaned_data['url']
+        image_name = '{}.{}'.format(slugify(image.title),
+                                  image_url.rsplit('.', 1)[1].lower())
+
+        # download image from the given URL
         response = request.urlopen(image_url)
-        image.image.save(image_name), ContentFile(response.read(), save=False)
+        image.image.save(image_name,
+                         ContentFile(response.read()),
+                         save=False)
         if commit:
             image.save()
         return image
